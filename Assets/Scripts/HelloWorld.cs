c4136b1621992ee39890aeb822a018fa5a75b92b
Hanif <andque12345012@gmail.com>
2024-10-09 23:58:30 +0700

    Add sandbox Scene so conflict could be avoid(i hope)

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : Interactable
+{
+//     public override void Interact()
+//     {
+//         Debug.Log("Hello World!");
+    // }/
+
+}
