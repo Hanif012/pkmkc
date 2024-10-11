d00547f49f1825d7250c6f9d4f4a42acbb96fdff
Bryant Chandra <bryantchandra2806@gmail.com>
2024-10-11 15:38:20 +0700

    animasi OnClick oven

--- Assets/Scripts/Open.cs
+++ Assets/Scripts/Open.cs
@@ -14,4 +14,38 @@ public class Open : MonoBehaviour
         
     }
     
+    private void OnMouseDown()
+    {
+        if (gameObject.name == "ovenDoor")
+        {
+            if(isOpen == "n")
+            {
+                Debug.Log("pp");
+                GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 2, 0);
+                isOpen = "o";
+                StartCoroutine(stopDoor());
+            }
+            else if (isOpen == "y")
+            {
+                GetComponent<Rigidbody>().angularVelocity = new Vector3(0, -2, 0);
+                isOpen = "c";
+                StartCoroutine(stopDoor());
+            }
+        }
+    }
+
+    IEnumerator stopDoor()
+    {
+        yield return new WaitForSeconds(5);
+
+        if (isOpen == "o")
+        {
+            isOpen = "y";
+        }
+        
+        if (isOpen == "c")
+        {
+            isOpen = "n";
+        }
+    }
 }
