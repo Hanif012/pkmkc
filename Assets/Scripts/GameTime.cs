2a19f03fe282ccc979bcc6644022f5d0482417c5
Hanif <andque12345012@gmail.com>
2024-10-17 15:55:32 +0700

    Add Time function

--- Assets/Scripts/GameTime.cs
+++ Assets/Scripts/GameTime.cs
@@ -2,29 +2,29 @@ using UnityEngine;
 
 public partial class GameTime
 {
+    public int TotalTime;
+    public int hour;
+    public int minute;
 
-        public int hour;
-        public int minute;
+    public GameTime(int hour, int minute)
+    {
+        this.hour = hour;
+        this.minute = minute;
+    }
 
-        public GameTime(int hour, int minute)
+    public void AddTime(int hour, int minute)
+    {
+        this.hour += hour;
+        this.minute += minute;
+        if (this.minute >= 60)
         {
-            this.hour = hour;
-            this.minute = minute;
+            this.minute -= 60;
+            this.hour++;
         }
+    }
 
-        public void AddTime(int hour, int minute)
-        {
-            this.hour += hour;
-            this.minute += minute;
-            if (this.minute >= 60)
-            {
-                this.minute -= 60;
-                this.hour++;
-            }
-        }
-
-        public void GetTime()
-        {            
-            Debug.Log(hour + ":" + minute);
-        }
+    public int GetTime()
+    {
+        return hour * 60 + minute;
+    }
 }
