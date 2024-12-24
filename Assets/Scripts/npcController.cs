32bf70a2dd043f3a4b069df4cc2d236de408e158
Bryant Chandra <bryantchandra2806@gmail.com>
2024-12-24 18:46:44 +0700

    Buggy anim

--- Assets/Scripts/npcController.cs
+++ Assets/Scripts/npcController.cs
@@ -13,10 +13,10 @@ public class npcController : MonoBehaviour
     public float minDistance = 0;    
     public int index = 0;
 
-    // AudioManager audioManager;
+    NewAudioManager audioManager;
     // private void Awake()
     // {
-    //     audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    //     audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<NewAudioManager>();
     // }
     void Start()
     {
@@ -42,21 +42,33 @@ public class npcController : MonoBehaviour
     {
         if (Vector3.Distance(transform.position, PathPoints[index].position) < minDistance)
         {
-            if (index + 1 != PathPoints.Length)
+            if (index == 4)
             {
+                StartCoroutine(PlayOpenDoorAnimation());
                 index++;
+            }
+            else if (index == PathPoints.Length - 1)
+            {
+                StartCoroutine(PlayWavingAnimation());
+                index++;
+            }
+            else if (index > PathPoints.Length - 1)
+            {
+                agent.isStopped = true;
+                animator.SetFloat("vertical", 0);
+                animator.SetFloat("horizontal", 0);         
             }
             else
             {
-                index = 0;
-                StartCoroutine(PlayWavingAnimation());
-                return;
+                index++;
             }
         }
 
         agent.SetDestination(PathPoints[index].position);
-
-        animator.SetFloat("vertical", !agent.isStopped ? 1 : 0);
+        if(!agent.isStopped)
+        {
+            animator.SetFloat("vertical", 1);
+        }
     }
 
     IEnumerator PlayWavingAnimation()
@@ -65,9 +77,25 @@ public class npcController : MonoBehaviour
         animator.SetFloat("vertical", 0);
         animator.SetFloat("horizontal", -1);
 
+        
         yield return new WaitForSeconds(3.5f);
 
         animator.SetFloat("horizontal", 0);
         agent.isStopped = false;
+    }
+
+    IEnumerator PlayOpenDoorAnimation()
+    {
+        agent.isStopped = true;
+        animator.SetFloat("vertical", -1);
+        animator.SetFloat("horizontal", 0);
+
+        yield return new WaitForSeconds(5.3f);
+
+        agent.isStopped = false;
+
+        animator.SetFloat("vertical", 1);
+
+        
     }
 }
